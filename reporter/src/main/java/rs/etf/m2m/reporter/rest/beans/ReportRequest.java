818967bc3ee4b833ddc4fe63fd7a2cf395ce14c1
Nikola <sjovac@gmail.com>
2018-08-29 00:32:42 +0200

    Final polishing

--- reporter/src/main/java/rs/etf/m2m/reporter/rest/beans/ReportRequest.java
+++ reporter/src/main/java/rs/etf/m2m/reporter/rest/beans/ReportRequest.java
@@ -11,7 +11,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@ToString
 @Data
 @Builder
 @NoArgsConstructor
